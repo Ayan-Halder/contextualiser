c6ecf2345baa2680067e8c08e260405afc3dcc5a
Ayan Halder <ayanhalder194@gmail.com>
2021-12-10 14:44:56 +0530

    edited index.js

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function wordcount(self) {
-    var spaces = self.value.match(/\S+ /g);
-    var words = spaces ? spaces.length : 0;
-    
-    document.getElementById('word-count').innerHTML = words+1 + " words";
-    document.getElementById('clist').innerHTML = document.getElementById('contenttext').value;
-    var content = document.getElementById('contenttext').value;
-    content.replace(/\n/g, " ");
-    var contentList = document.getElementById('clist').innerHTML.split(" ");
-    console.log(contentList);
+const stopwords = ['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now']
+
+function firstLetterUpper(theString) {
+	var newString = theString.replace(/(^\s*\w|[\.\!\?]\s*\w)/g,function(c){return c.toUpperCase()});
+  return newString;
 }
+
+function removeDuplicates(contentArray){
+    ContentString = new Set(contentArray);
+    console.log(new Set(contentArray));
+}
+
+function wordcount(self) {
+    var content = document.getElementById('contenttext').value;
+    content = content.replace(/  /g, " ");
+    contentstring = content.replace(/\n/g, " ");
+    var contentList = contentstring.split(" ");
+    console.log(contentList);
+    document.getElementById('word-count').innerHTML = contentList.length;
+    document.getElementById('contentKeywords').innerHTML = firstLetterUpper(content);
+}
+
+function removestopwords() {
+    res = []
+    contentstring = contentstring.replace(/[.,\/#!$%\^&\*;:{}=\-_`~?()]/g,"");
+    contentstring = contentstring.toLowerCase();
+    words = contentstring.split(' ')
+    for(i=0;i<words.length;i++) {
+       word_clean = words[i].split(".").join("")
+       if(!stopwords.includes(word_clean)) {
+           res.push(word_clean)
+       }
+    }
+    document.getElementById('contentKeywords').innerHTML = (res.join(' '));
+    document.getElementById('clist').innerHTML = res.length;
+    removeDuplicates(res);
+}  
+
